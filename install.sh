b119f5050eafb4ed2ec2a020433530a94eed1837
V1nic1us <marcusvinicius.mds16@gmail.com>
2024-10-23 08:58:06 -0400

    add: install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+# Install sdkman
+curl -s "https://get.sdkman.io" | bash
+
+# Install golang
+sudo apt install golang-1.23-go
